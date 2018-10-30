04c24ef109fe2880be069d1017bc04c4cced1d13
krystian <krystian.piatek92@gmail.com>
2018-10-30 19:50:59 +0100

    Commit message

---
+++ src/WordsCount.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WordsCount {
+    public static void main(String[] args) throws FileNotFoundException {
+        String word = JOptionPane.showInputDialog("Podaj szukane słowo ");
+        JFileChooser chooser = new JFileChooser();
+chooser.setCurrentDirectory(new java.io.File("C:\\Users\\krystian\\Desktop"));
+chooser.setDialogTitle("Wybierz plik z tekstem");
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            Scanner sc = new Scanner(file);
+            int counter = 0;
+            while (sc.hasNext()) {
+                if (sc.next().equals(word)) {
+                    counter++;
+                }
+            }
+            System.out.println("Słowo " + word + " wystąpiło w wybranym tekście "+counter+ " razy");
+        }
+    }
+}
